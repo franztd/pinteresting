cd43e9c4fb11c9e12c98086aefc8d123e44bae9b
Tim Franz <franztd@gmail.com>
2014-08-07 13:49:11 -0400

    Updated paperclip initializer

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -6,3 +6,6 @@ module Paperclip
     end
   end
 end
+
+Paperclip::Attachment.default_options[:url] = 'pinteresting-franztd.s3.amazonaws.com'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
