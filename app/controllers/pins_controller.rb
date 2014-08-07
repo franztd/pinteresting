466ac33594039db6b7919c74d900ebe453785f82
Tim Franz <franztd@gmail.com>
2014-08-06 22:26:05 -0400

    Add pins/users association

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,27 +1,24 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
-  # GET /pins
   def index
     @pins = Pin.all
   end
 
-  # GET /pins/1
   def show
   end
 
-  # GET /pins/new
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
   end
 
-  # GET /pins/1/edit
   def edit
   end
 
-  # POST /pins
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
 
     if @pin.save
       redirect_to @pin, notice: 'Pin was successfully created.'
@@ -30,7 +27,6 @@ class PinsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /pins/1
   def update
     if @pin.update(pin_params)
       redirect_to @pin, notice: 'Pin was successfully updated.'
@@ -39,17 +35,21 @@ class PinsController < ApplicationController
     end
   end
 
-  # DELETE /pins/1
   def destroy
     @pin.destroy
     redirect_to pins_url, notice: 'Pin was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+   # Use callbacks to share common setup or constraints between actions.
     def set_pin
       @pin = Pin.find(params[:id])
     end
+
+    def correct_user
+      @pin = current_user.pins.find_by(params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+    end  
 
     # Only allow a trusted parameter "white list" through.
     def pin_params
